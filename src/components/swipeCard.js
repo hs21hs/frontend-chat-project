7ce1ad4f52dd40b53550e4022ccf551c28531ba7
github username <github email address>
2020-02-02 22:13:07 +0000

    starting getMyMatches endpoint

--- src/components/swipeCard.js
+++ src/components/swipeCard.js
@@ -2,22 +2,31 @@ import React, {Component} from 'react';
 
 class SwipeCard extends Component {
 
-  
+  showCard = () => {
+    if (this.props.state.swipeUsers.length > 0){
+      return(
+        <div class = "row swipeCard">
+        <img class = "swipeCardImg" src = {this.props.state.swipeUsers[0].image_url} alt =""/>
+        <div>
+          <p>name:{this.props.state.swipeUsers[0].username}</p>
+          <p>age:{this.props.state.swipeUsers[0].age}</p>
+          <p>breed:{this.props.state.swipeUsers[0].breed}</p>
+        </div>
+        <div>
+          <button onClick = {() => {this.props.like(this.props.state.swipeUsers[0])}}>like</button>
+          <button onClick = {() => {this.props.dislike(this.props.state.swipeUsers[0])}}>dislike</button>
+        </div>
+        
+      </div>
+      )
+    }else{return(<p>out of users to swipe!</p>)}
+  }
+
   render () {
     return (
-    <div class = "row swipeCard">
-      <img class = "swipeCardImg" src = {this.props.state.swipeUsers[0].image_url} alt =""/>
       <div>
-        <p>name:{this.props.state.swipeUsers[0].username}</p>
-        <p>age:{this.props.state.swipeUsers[0].age}</p>
-        <p>breed:{this.props.state.swipeUsers[0].breed}</p>
+        {this.showCard()}
       </div>
-      <div>
-        <button>like</button>
-        <button onClick = {() => {this.props.dislike(this.props.state.swipeUsers[0])}}>dislike</button>
-      </div>
-      
-    </div>
     )
   }
 }
